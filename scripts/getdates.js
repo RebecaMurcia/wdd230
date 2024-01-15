f05ab88462f1006a385a2d3ec68da876b3e06b16
RebecaMurcia <lop17010@byui.edu>
2024-01-14 23:08:30 -0800

    w1

---
+++ scripts/getdates.js
@@ -0,0 +1,4 @@
+//Current year
+document.getElementById("year").innerHTML = new Date().getFullYear();
+//Last modified 
+document.getElementById('lastModified').innerHTML = new Date(document.lastModified);
