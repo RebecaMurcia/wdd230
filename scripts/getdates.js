215d5ce735fd22e1e7d0eff65afecb3e059a2017
RebecaMurcia <lop17010@byui.edu>
2024-02-03 21:22:47 -0800

    w4

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,3 +2,13 @@
 document.getElementById("year").innerHTML = new Date().getFullYear();
 //Last modified 
 document.getElementById('lastModified').innerHTML = new Date(document.lastModified);
+
+//navigation bar
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+}
+);
