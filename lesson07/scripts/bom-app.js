97257d191761d8bb5fc6b666e89df12765cf45c8
RebecaMurcia <lop17010@byui.edu>
2024-02-21 23:11:19 -0800

    w7

--- lesson07/scripts/bom-app.js
+++ lesson07/scripts/bom-app.js
@@ -23,34 +23,22 @@ button.addEventListener('click', () => {
 
 
 function displayList(item) {
-    let li = document.createElement('li');
-    let deleteButton = document.createElement('button');
-    li.textContent = item;
-    deleteButton.textContent = '❌ ';
-    deleteButton.classList.add('delete');
-    list.append(li);
-    deleteButton.addEventListener('click', function () {
-        list.removeChild(li);
-        deleteChapter(li.textContent);
-        input.focus(); 
-    });  
-    console.log('I like typing code and trying to understand it.') 
+        let li = document.createElement('li');
+        let deleteButton = document.createElement('button');
 
-    // if (input.value != '') {
-    //     const li = document.createElement('li');
-    //     const deleteButton = document.createElement('button');
-
-    //     li.textContent = input.value;
-    //     deleteButton.textContent = '❌ ';
-    //     li.append(deleteButton);
-    //     list.append(li);
-    //     deleteButton.addEventListener('click', function () {
-    //         list.removeChild(li);
-
-    //     });
-    //     input.focus();
-    //     input.value = '';
+        li.textContent = item;
+        deleteButton.textContent = '❌ ';
+        deleteButton.classList.add('delete');
+        li.append(deleteButton);
+        list.append(li);
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+            deleteChapter(li.textContent);
+            input.focus();
+        });
+       console.log('I like to copy instead of typing it out myself and trying to understand it.');
     }
+
 
 function setChapterList() {
     localStorage.setItem('myFavBOMList', JSON.stringify(chaptersArray));
@@ -59,10 +47,10 @@ function setChapterList() {
 function getChapterList() {
     return JSON.parse(localStorage.getItem('myFavBOMList'));
 }
-chapter = chapter.slice(0, chapter.length - 1);
+chapter = chapter.slice(0, chapter.length -1);
 chaptersArray = chaptersArray.filter((item) => item !== chapter);
 function deleteChapter(chapter) {
-    chapter = chapter.slice(0, chapter.length - 1);
+    chapter = chapter.slice(0, chapter.length -1);
     chaptersArray = chaptersArray.filter(item => item !== chapter);
     setChapterList();
-}
+};
