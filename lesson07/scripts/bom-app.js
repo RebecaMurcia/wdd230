4ad581e1edb23bd4af0f0a697730d282e020423d
RebecaMurcia <lop17010@byui.edu>
2024-02-21 22:17:54 -0800

    w7

--- lesson07/scripts/bom-app.js
+++ lesson07/scripts/bom-app.js
@@ -1,25 +1,68 @@
 //input, button, and list variables
 const input = document.querySelector('#favchap');
-const button=document.querySelector('button');
+const button = document.querySelector('button');
 const list = document.querySelector('#list');
 
-//event listener & function
-button.addEventListener('click',() => {
+let chaptersArray = getChapterList() || [];
 
-if(input.value != '') {  }
-    const li = document.createElement('li');
-    const deleteButton = document.createElement('button');
+//event listener & functions
+chaptersArray.forEach(chapter => {
+    displayList(chapter);
+});
 
-    li.textContent = input.value;
+button.addEventListener('click', () => {
+
+    if (input.value != '') {
+        display(input.value);
+        chaptersArray.push(input.value);
+        setChapterList();
+        input.value = '';
+        input.focus();
+    }
+});
+
+
+function displayList(item) {
+    let li = document.createElement('li');
+    let deleteButton = document.createElement('button');
+    li.textContent = item;
     deleteButton.textContent = '❌ ';
-    li.append (deleteButton);
+    deleteButton.classList.add('delete');
     list.append(li);
     deleteButton.addEventListener('click', function () {
-       list.removeChild(li);
-        
-    });
-    input.focus ();
-    input.value = '';
+        list.removeChild(li);
+        deleteChapter(li.textContent);
+        input.focus(); 
+    });  
+    console.log('I like typing code and trying to understand it.') 
 
-}); 
+    // if (input.value != '') {
+    //     const li = document.createElement('li');
+    //     const deleteButton = document.createElement('button');
 
+    //     li.textContent = input.value;
+    //     deleteButton.textContent = '❌ ';
+    //     li.append(deleteButton);
+    //     list.append(li);
+    //     deleteButton.addEventListener('click', function () {
+    //         list.removeChild(li);
+
+    //     });
+    //     input.focus();
+    //     input.value = '';
+    }
+
+function setChapterList() {
+    localStorage.setItem('myFavBOMList', JSON.stringify(chaptersArray));
+}
+
+function getChapterList() {
+    return JSON.parse(localStorage.getItem('myFavBOMList'));
+}
+chapter = chapter.slice(0, chapter.length - 1);
+chaptersArray = chaptersArray.filter((item) => item !== chapter);
+function deleteChapter(chapter) {
+    chapter = chapter.slice(0, chapter.length - 1);
+    chaptersArray = chaptersArray.filter(item => item !== chapter);
+    setChapterList();
+}
