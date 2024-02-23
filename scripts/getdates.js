5e6a81e207fc0e7050669312f1cae9c149f826ff
RebecaMurcia <lop17010@byui.edu>
2024-02-22 21:52:59 -0800

    w7

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -12,3 +12,19 @@ hamButton.addEventListener('click', () => {
     hamButton.classList.toggle('open');
 }
 );
+//Number of visits (Local Storage)
+const visitsDisplay =
+document.querySelector('.visits');
+
+let numVisits =
+Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+if (numVisits !== 0) {
+    visitsDisplay.textContent = numVisits;
+} else {
+    visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
+}
+
+numVisits++;
+
+localStorage.setItem("numVisits-ls", numVisits);
