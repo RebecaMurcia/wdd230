972b83e3278f496019cc1ea0ce772e83e44a7a23
RebecaMurcia <lop17010@byui.edu>
2024-02-24 21:27:53 -0800

    w7

--- chamber/scripts/chamber-scripts.js
+++ chamber/scripts/chamber-scripts.js
@@ -20,3 +20,20 @@ modeButton.addEventListener("click", () => {
 	body.classList.toggle('dark-mode');
 	main.classList.toggle('dark-mode');
 });
+// number of visits
+const visitsDisplay =
+document.querySelector('.number-visits');
+
+let numVisits =
+Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+if (numVisits !== 0) {
+    visitsDisplay.textContent = numVisits;
+} else {
+    visitsDisplay.textContent = `Welcome! Let us know if you have any questions.`;
+}
+
+numVisits++;
+
+localStorage.setItem("numVisits-ls", numVisits);
+
