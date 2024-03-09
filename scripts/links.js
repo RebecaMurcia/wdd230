98f6faa09d25dc3aa896ff397608323a7a5cd382
RebecaMurcia <lop17010@byui.edu>
2024-03-09 12:37:55 -0800

    w9

--- scripts/links.js
+++ scripts/links.js
@@ -1,9 +1,36 @@
 const baseURL = "https://rebecamurcia.github.io/wdd230/";
 const  linksURL = "https://rebecamurcia.github.io/wdd230/data/links.json";
 
-async function getLinks() {
+async function getLinks () {
     const response = await fetch(linksURL);
     const data = await response.json();
-    console.log(data);
+    displayLinks(data);
 }
 getLinks();
+
+const displayLinks = (weeks) => {
+    weeks.forEach ((week) => {
+        //console.log(week);
+        const listItem = document.createElement("li");
+        const lessonWeek = document.createTextNode(`Lesson ${week.lesson}:`);
+
+        listItem.appendChild(lessonWeek);
+
+        week.links.forEach((link) => {
+            //console.log(link);
+             const rag = document.createElement("a");
+             EventTarget.setAttribute("href", link.url);
+             EventTarget.textContent = link.title;
+
+             listItem.appendChild(tag);
+            if (week.link.length > 1 && week.links.indexOf(link) < week.links.length - 1){
+                listIten.innerHTML += " | ";
+            }    
+        });
+        const list = document.querySelector(".la-card");
+        list.appendChild(listItem);
+    });
+};
+
+
+    
