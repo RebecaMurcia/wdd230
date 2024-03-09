2e82dd44addc6551598f6ba896a0457b660d72a4
RebecaMurcia <lop17010@byui.edu>
2024-03-08 19:41:26 -0800

    w9

--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -1,9 +1,45 @@
 const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
-
 const cards = document.querySelector('#cards');
 async function getProphetData() {
-    const response = await fetch(url);
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error ('Failed to fetch data');
+        }
     const data = await response.json();
-    console.table(data.prophets);
+    displayProphets(data.prophets);
+    return data;
+    }catch (error) {
+        console.error('Error fetching data:', error);
+        throw error;
+    }    
 }
 getProphetData();
+
+const displayProphets = (prophets) =>{
+    prophets.forEach ((prophet) => {
+        let card = document.createElement('section');
+        let fullName = document.createElement('h2');
+        let birthdate = document.createElement('p');
+        let birthplace = dosument.createElement('p');
+        let portrait = document.createElement('img');
+        
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+        birthdate.textContent = `Date of Birth: ${prophet.birthdate}`;
+        birthplace.textContent = `Place of Birth: ${prophet.birthplace}`;
+
+        portrait.setAttribute('src',prophet.imageurl);
+        portrait.setAttribute('alt', prophet.fullName);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '340');
+        portrait.setAttribute('height', '440');
+
+        card.appendChild(fullName);
+        card.appendChild(birthdate);
+        card.appendChild(birthplace);
+        card.appendChild(portrait);
+        
+        cards.appendChild(card);
+    });
+
+};
