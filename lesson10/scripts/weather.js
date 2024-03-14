2888e947f7a8ea26098188f1aa430c05f8f659fa
RebecaMurcia <lop17010@byui.edu>
2024-03-13 17:23:37 -0700

    w10

--- lesson10/scripts/weather.js
+++ lesson10/scripts/weather.js
@@ -3,20 +3,33 @@ const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 const url ='https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&appid=f39cf9a5e290c08455b011260c79a5de&units=imperial';
 
-async function apiFetch(){
-    try{
+async function apiFetch() {
+    try {
         const response = await fetch(url);
         if (response.ok) {
             const data = await response.json();
             console.log(data);
-            displayResults(data);
-
+        displayResults(data); 
         } else {
             throw Error(await response.text());
         }
-
-    } catch (error){
+    }catch (error) {
         console.log(error);
     }
 }
 apiFetch();
+
+function displayResults(data){
+    const iconURL = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+weatherIcon.setAttribute("src", iconURL); 
+captionDesc.textContent = data.weather[0].description;
+currentTemp.textContent = " "+ Math.round(data.main.temp);
+}
+
+
+
+
+
+
+
+
