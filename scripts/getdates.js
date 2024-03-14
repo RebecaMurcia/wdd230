efd76fcf56690f7ee64017826d6c286b92f31475
RebecaMurcia <lop17010@byui.edu>
2024-03-13 19:40:19 -0700

    w10

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -29,15 +29,15 @@ numVisits++;
 
 localStorage.setItem("numVisits-ls", numVisits);
 
-//form re-enter password results
-// This should be refactored.
+//form re-enter password results****************
+// This should be refactored********************
 const p1 = document.querySelector("#pssw");
 const p2 = document.querySelector("#rpssw");
 const message = document.querySelector("#formmessage");
 
-p2.addEventListener("focusout", checkSame);
+// p2.addEventListener("focusout", checkSame);
 
-// This should be refactored.
+// This should be refactored*********************
 function checkSame() {
 	if (p1.value !== p2.value) {
 		message.textContent = "❗Key Phrases DO NOT MATCH!";
@@ -51,18 +51,18 @@ function checkSame() {
 		p2.style.color = "#000";
 	}
 }
-//email validation
+//email validation***************************
 document.addEventListener("DOMContentLoaded", function () {
 	// Function to handle form submission
 	function handleSubmit(event) {
-		// Prevent default form submission
+		// Prevent default form submission*********
 		event.preventDefault();
 
-		// Access form elements
+		// Access form elements********************
 		let formt = event.target;
-		let formData = new FormData(formt);
+		let formData = new FormData(form);
 
-		// Display form element values
+		// Display form element values****************
 		for (let pair of formData.entries()) {
 			console.log(pair[0] + ": " + pair[1]);
 		}
@@ -71,16 +71,45 @@ document.addEventListener("DOMContentLoaded", function () {
 	const form = document.querySelector("form");
 	form.addEventListener("submit", handleSubmit);
 });
-//page range section
+//page range section*****************************
 const rangevalue = document.getElementById("rangevalue");
 const range = document.getElementById("r");
 
-// RANGE event listener
-range.addEventListener('change', displayRatingValue);
-range.addEventListener('input', displayRatingValue);
+// RANGE event listener**************************
+// range.addEventListener("change", displayRatingValue);
+// range.addEventListener("input", displayRatingValue);
 
 function displayRatingValue() {
     rangevalue.innerHTML = range.value;
 }
+ //Weather section***********************
+ const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+const url ='https://api.openweathermap.org/data/2.5/weather?lat=45.63&lon=122.66&appid=f39cf9a5e290c08455b011260c79a5de&units=imperial';
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+        displayResults(data); 
+        } else {
+            throw Error(await response.text());
+        }
+    }catch (error) {
+        console.log(error);
+    }
+}
+apiFetch();
+
+function displayResults(data){
+    const iconURL = "images/clouds.png";
+weatherIcon.setAttribute("src", iconURL); 
+captionDesc.textContent = data.weather[0].description;
+currentTemp.textContent = " "+ Math.round(data.main.temp);
+}
+
 
 
