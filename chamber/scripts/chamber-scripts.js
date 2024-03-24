540ee5f0ab16d6a06d12adc7aaebb2f2612b1bb1
RebecaMurcia <lop17010@byui.edu>
2024-03-23 21:52:47 -0700

    w11

--- chamber/scripts/chamber-scripts.js
+++ chamber/scripts/chamber-scripts.js
@@ -30,7 +30,7 @@ let numVisits =
 Number(window.localStorage.getItem("numVisits-ls")) || 0;
 
 if (numVisits !== 0) {
-    // visitsDisplay.textContent = numVisits;
+    visitsDisplay.textContent = numVisits;
 } else {
     visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
 }
