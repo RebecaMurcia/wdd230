ef2447f1ee8e60bb61304ebb417c097649361575
RebecaMurcia <lop17010@byui.edu>
2024-04-01 19:50:21 -0700

    w13

---
+++ scoots/scripts/indexscripts.js
@@ -0,0 +1,9 @@
+//navigation bar****************************************
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+}
+);
