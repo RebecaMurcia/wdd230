78d3a67c11ddf210bc0cb7ced219817b57b9041c
RebecaMurcia <lop17010@byui.edu>
2024-04-04 01:22:57 -0700

    w13

--- scoots/scripts/weather.js
+++ scoots/scripts/weather.js
@@ -1,11 +1,15 @@
 const currentTemp = document.querySelector('#temperature');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('#weather-description');
+const todayHumidity = document.querySelector('#today-humd');
+const todayWind = document.querySelector('#today-wind');
 
 const forecastTemp1 = document.querySelector('#temp-forecast1');
 const weatherIcon1 = document.querySelector('#icon1');
 const forecastDay1 = document.querySelector('#day1');
 const forecastDesc = document.querySelector('#day1-desc');
+const foreHumidity = document.querySelector('#day1-humd');
+const foreWind = document.querySelector('#day1-wind');
 
 const url ='https://api.openweathermap.org/data/2.5/weather?lat=20.62847&lon=-87.07355&appid=f39cf9a5e290c08455b011260c79a5de&units=imperial';
 const furl='https://api.openweathermap.org/data/2.5/forecast?lat=20.62847&lon=-87.07355&appid=f39cf9a5e290c08455b011260c79a5de&units=imperial';
@@ -33,11 +37,14 @@ function displayResults(data){
 weatherIcon.setAttribute("src", iconURL); 
 captionDesc.textContent = data.weather[0].description;
 currentTemp.textContent = " "+ Math.round(data.main.temp);
+todayHumidity.textContent = data.main.humidity;
+todayWind.textContent = data.wind.speed.toFixed();
+
 }
 
 async function forecastFetch() {
     try {
-        const response =await fetch(furl);
+        const response =await fetch(furl); 
         if(response.ok) {
             const data =await response.json();
             console.log(data);
@@ -58,7 +65,8 @@ function displayForecast(data) {
     weatherIcon1.setAttribute("src",iconsrc);
     weatherIcon1.setAttribute("alt", "weather icon"); 
     forecastDesc.textContent =`${data.list[6].weather[0].description}`;
-    
+    foreHumidity.textContent = `${data.list[6].main.humidity}`;
+    foreWind.textContent = `${data.list[6].wind.speed.toFixed()}`;
 }
 
 function forecastDays() {
