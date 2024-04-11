eaa199ce5370817b971da21584fb352641a09440
RebecaMurcia <lop17010@byui.edu>
2024-04-10 18:42:28 -0700

    w13

--- scoots/scripts/table.js
+++ scoots/scripts/table.js
@@ -1,59 +1,30 @@
 const baseURL = "https://rebecamurcia.github.io/wdd230/";
-const dataURL = "../data/members.json";
+const dataURL = "price.json";
 
-const displayMembers = (members) => {
-    const cards = document.querySelector("#members");
-     members.forEach((member) => {
-        const bizCard = document.createElement("section");
-        bizCard.setAttribute("class", "member");
-        const companyName = document.createElement("h3");
-        companyName.textContent = member.name;
-        const address = document.createElement("p");
-        address.textContent = member.address;
-        const phone = document.createElement("p");
-        phone.textContent = member.phone;
-        const website = document.createElement("a");
-        website.setAttribute("href", `https://${member.website}`);
-        website.textContent = member.website;
+function buildTable(dataURL){
+    var table = document.getElementById('myTable')
 
-        const logo = document.createElement("img");
-        logo.setAttribute("src", member.logo);
-        logo.setAttribute("alt", `${member.name.toLowerCase()}-logo`);
-        logo.setAttribute("loading", "lazy");
-        logo.setAttribute("width", "75");
-        logo.setAttribute("height", "auto");
+    for(var i = 0; i < dataURL.length; i++){
+        var row = `<tr>
+                      <td>${dataURL[i].type}</td>
+                      <td>${dataURL[i].capacity}</td>
+                      <td>${dataURL[i].halfrsv}</td>
+                      <td>${dataURL[i].fullrsv}</td>
+                      <td>${dataURL[i].halfwlkn}</td>
+                      <td>${dataURL[i].fullwlkn}</td>
+                   </tr> `
+        table.innerHTML += row
 
-        bizCard.appendChild(logo);
-        bizCard.appendChild(companyName);
-        bizCard.appendChild(address);
-        bizCard.appendChild(phone);
-        bizCard.appendChild(website);
-
-        cards.appendChild(bizCard);
-     });
-};
-const gridbutton = document.querySelector("#grid");
-const listbutton = document.querySelector("#list");
-const display = document.querySelector("article");
-
-gridbutton.addEventListener("click", () => {
-    display.classList.add("grid");
-    display.classList.remove("list");
-});
-
-listbutton.addEventListener("click",showList);
-
-function showList(){
-    display.classList.add("list");
-    display.classList.remove("grid");
+    }
 }
+       
 
-fetch(dataURL).then(function(response) {
-    return response.json();
-}).then(function(jsonObject) {
-    // console.table(jsonObject)
-    const buzzList = jsonObject['members'];
- buzzList.forEach(displayMembers);
- console.log("JSON work");
-});
-console.log("test");
+// fetch(dataURL).then(function(response) {
+//     return response.json();
+// }).then(function(jsonObject) {
+//     // console.table(jsonObject)
+//     const buzzList = jsonObject['members'];
+//  buzzList.forEach(displayMembers);
+//  console.log("JSON work");
+// });
+// console.log("test");
