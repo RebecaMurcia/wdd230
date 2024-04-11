6f3bdd1bd9e7f7d5500aec0e5145a80f3b8fa1d3
RebecaMurcia <lop17010@byui.edu>
2024-04-10 19:46:38 -0700

    w13

--- scoots/scripts/table.js
+++ scoots/scripts/table.js
@@ -1,28 +1,69 @@
-const baseURL = "https://rebecamurcia.github.io/wdd230/";
+const baseURL = "https://rebecamurcia.github.io/wdd230/scoots/price.json";
 const dataURL = "price.json";
-
-function buildTable(dataURL){
-    var table = document.getElementById('myTable')
-
-    for(var i = 0; i < dataURL.length; i++){
-        var row = `<tr>
-                      <td>${dataURL[i].type}</td>
-                      <td>${dataURL[i].capacity}</td>
-                      <td>${dataURL[i].halfrsv}</td>
-                      <td>${dataURL[i].fullrsv}</td>
-                      <td>${dataURL[i].halfwlkn}</td>
-                      <td>${dataURL[i].fullwlkn}</td>
-                   </tr> `
-        table.innerHTML += row
-
-    }
-}
        
+ async function price(){
+    try{
+        const response = await fetch(dataURL);
+        if (response.ok) {
+            const data = await response.json();
+            displayTable(data.price);
+        } else{
+            throw Error(await response.text()); 
+        }
+    } catch (error) {
+        console.log(error);
+    }
+ }
+ price();
 
-// fetch(dataURL).then(function(response) {
+ const displayTable = (price) =>{
+    console.log(price);
+    const priceTable = document.getElementById("myTable");
+
+    price.forEach((type) => {
+        const row = document.createElement("tr");
+        row.innerHTML = `
+             <td>${type.type}</td>
+             <td>${type.capacity}</td>
+             <td>$${type.halfrsv}</td>
+             <td>$${type.fullrsv}</td>
+             <td>$${type.halfwlkn}</td>
+             <td>$${type.fullwlkn}</td>      
+        
+        `;
+        priceTable.appendChild(row);
+    });
+ };
+
+
+
+
+
+
+
+
+// function displayTable(price){
+//     var table = document.getElementById('myTable')
+
+//     for(var i = 0; i < dataURL.length; i++){
+//         var row = `<tr>
+//                       <td>${dataURL[i].type}</td>
+//                       <td>${dataURL[i].capacity}</td>
+//                       <td>${dataURL[i].halfrsv}</td>
+//                       <td>${dataURL[i].fullrsv}</td>
+//                       <td>${dataURL[i].halfwlkn}</td>
+//                       <td>${dataURL[i].fullwlkn}</td>
+//                    </tr> `
+//         table.innerHTML += row
+//     }
+// }
+
+
+
+ // fetch(dataURL).then(function(response) {
 //     return response.json();
 // }).then(function(jsonObject) {
-//     // console.table(jsonObject)
+//     console.table(jsonObject)
 //     const buzzList = jsonObject['members'];
 //  buzzList.forEach(displayMembers);
 //  console.log("JSON work");
